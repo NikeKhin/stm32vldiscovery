bc496d81647960742c8bd6b8c6210c7f42041d3d
Nike Khin <nikmk@gmx.de>
2014-03-05 23:47:46 +0400

    SPL stubs for Pin were added

--- inc/pin.h
+++ inc/pin.h
@@ -4,7 +4,7 @@
 #include "common.h"
 #include "bus.h"
 
-template<PeripheralID pid, typename T_busname>
+template<PeripheralID id, typename T_busname>
 class Port: public Device<T_busname>
 {
     //some static staff (common for all class instances) may come here
@@ -14,46 +14,37 @@ protected:
 public:
     Port(PeripheralID pid);
 
-    uint16_t read();
-    void write(uint16_t value);
+    uint16_t read(){
+        //uint16_t GPIO_ReadInputData(GPIO_TypeDef* GPIOx);
+    }
+    void write(uint16_t value){
+        //void GPIO_Write(GPIO_TypeDef* GPIOx, uint16_t PortVal);
+    }
 };
 
-//TODO: check port-bus correspondence. It is definitely wrong
 using PortA = Port<PeripheralID::gpioa,Bus2>;
 using PortB = Port<PeripheralID::gpiob,Bus2>;
 using PortC = Port<PeripheralID::gpioc,Bus2>;
 using PortD = Port<PeripheralID::gpiod,Bus2>;
 using PortE = Port<PeripheralID::gpioe,Bus2>;
-//using PortF = Port<GPIOF,Bus2>;
-//using PortG = Port<GPIOG,Bus2>;
 
 
-
-template<class T_portname>
+template<typename T_portname>
 class Pin: public T_portname
 {
 protected:
-    uint16_t pin;
+    uint16_t _pin;
 public:
     Pin(uint8_t id){
-        this->pin = 0x0001 << id;
+        this->_pin = 0x0001 << id;
     };
-    void setupSlowOutAnalog()
-    {
-        GPIO_InitTypeDef GPIO_InitStructure;
-        GPIO_InitStructure.GPIO_Pin = pin;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-        //GPIO_Init(base, &GPIO_InitStructure);
-    };
-    void setupSlowInAnalog()
-    {
-        GPIO_InitTypeDef GPIO_InitStructure;
-        GPIO_InitStructure.GPIO_Pin = pin;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-        //GPIO_Init(base, &GPIO_InitStructure);
-    };
+    void set(bool value){
+        //void GPIO_WriteBit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, BitAction BitVal);
+    }
+    bool get(){
+        //uint8_t GPIO_ReadInputDataBit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+        //uint8_t GPIO_ReadOutputDataBit(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+    }
 };
 
 using PinA = Pin<PortA>;
@@ -61,7 +52,31 @@ using PinB = Pin<PortB>;
 using PinC = Pin<PortC>;
 using PinD = Pin<PortD>;
 using PinE = Pin<PortE>;
-//using PinF = Pin<PortF>;
-//using PinG = Pin<PortG>;
+
+
+template<typename T_portname>
+class InFloatingPin: public Pin<T_portname>
+{
+    InFloatingPin(uint8_t id):Pin<T_portname>(id){
+        GPIO_InitTypeDef GPIO_InitStructure;
+        GPIO_InitStructure.GPIO_Pin = Pin<T_portname>::_pin;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+        GPIO_Init(T_portname::base, &GPIO_InitStructure);
+    }
+};
+template<typename T_portname>
+class OutAnalogPin: public Pin<T_portname>
+{
+    OutAnalogPin(uint8_t id):Pin<T_portname>(id){
+        GPIO_InitTypeDef GPIO_InitStructure;
+        GPIO_InitStructure.GPIO_Pin = Pin<T_portname>::_pin;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+        GPIO_Init(T_portname::base, &GPIO_InitStructure);
+    }
+
+};
+
 
 #endif // PIN_H
--- src/pin.cpp
+++ src/pin.cpp
@@ -6,3 +6,31 @@ Port<PeripheralID::gpioa,Bus2>::Port(PeripheralID pid):
     base(GPIOA)
 {
 }
+
+template<>
+Port<PeripheralID::gpiob,Bus2>::Port(PeripheralID pid):
+    Device<Bus2>(pid),
+    base(GPIOB)
+{
+}
+
+template<>
+Port<PeripheralID::gpioc,Bus2>::Port(PeripheralID pid):
+    Device<Bus2>(pid),
+    base(GPIOC)
+{
+}
+
+template<>
+Port<PeripheralID::gpiod,Bus2>::Port(PeripheralID pid):
+    Device<Bus2>(pid),
+    base(GPIOD)
+{
+}
+
+template<>
+Port<PeripheralID::gpioe,Bus2>::Port(PeripheralID pid):
+    Device<Bus2>(pid),
+    base(GPIOE)
+{
+}
